9c38a33f9e47cdab52e5f805dae02147f8f8b066
martinpavan1 <pavanmartinoscar@gmail.com>
2025-08-05 17:15:06 -0300

    avance carrera done

--- siuguaraniFront/src/app/avancecarrera/avancecarrera.ts
+++ siuguaraniFront/src/app/avancecarrera/avancecarrera.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Materiasservice } from '../materiasservice/materiasservice';
+import { Localstorageservice } from '../localstorageservice/localstorageservice';
+import { Estado } from '../models/estadomodel';
 
 @Component({
   selector: 'app-avancecarrera',
@@ -7,5 +10,30 @@ import { Component } from '@angular/core';
   styleUrl: './avancecarrera.css'
 })
 export class Avancecarrera {
+  avance = 0
+  constructor(private materiaservice: Materiasservice, private lsservice: Localstorageservice){}
 
+  avanceCarrera(){
+    const studentID = this.lsservice.getLocalStorage()?.studentID
+  
+    if(studentID){
+      this.materiaservice.getMateriasPorAlumno(studentID).subscribe({
+        next: list=> {
+            const aprobadas = list.filter(it => it.estado == Estado.APROBADO).length
+            const totalMaterias = 30
+            this.avance = Math.round((aprobadas/totalMaterias)*100)
+
+        },
+        error: err=>{
+          console.error("Error al obetener el avance de carrera")
+        }
+      })
+    }
+
+  }
+
+
+  ngOnInit(){
+    this.avanceCarrera()
+  }
 }
